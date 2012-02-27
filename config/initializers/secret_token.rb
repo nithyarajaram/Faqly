2548a1111973dab8f152d0cc6a157a0f0579b2f3
Nithya Rajaram <nithya@supportbee.com>
2012-02-26 21:04:59 -0300

    Initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faqly::Application.config.secret_token = 'acbb190b740570c1ab0720778d5838bfc1eee7438d9950846c4ec21c467d9936b948c885db040594a51247b5e308103d287861536d72cf9d1aa7815bf09e1569'
